6e8043c941837bb617d1490a38b0e9c96bbabbf1
pvineet44@gmail.com <vparekh@e1r8p2.42.fr>
2020-02-05 12:43:05 +0100

    project dir

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,16 +6,17 @@
 /*   By: vparekh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 12:20:14 by vparekh           #+#    #+#             */
-/*   Updated: 2020/02/05 12:26:50 by vparekh          ###   ########.fr       */
+/*   Updated: 2020/02/05 12:40:50 by vparekh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "minishell.h"
 
 int main(int argc, const char *argv[])
 {
 	char *line;
 	int fd;
+	t_minishell_meta ms;
 
 	fd = 0;
 	line = NULL;
