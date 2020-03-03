734fad46d0d06d3793e43cf63a703c277c83d38f
Mohit ASHAR <mohitashar1995@gmail.com>
2020-03-03 13:00:37 +0100

    cd with home unset done. Execute norme also done

--- src/ms_execute.c
+++ src/ms_execute.c
@@ -31,6 +31,25 @@ static void		write_permission_denied(char *path)
 	errno = 126;
 }
 
+int				check_file_permission(char *path)
+{
+	struct stat file_stat;
+
+	if (stat(path, &file_stat) >= 0 && ((file_stat.st_mode & S_IXUSR) == 0))
+	{
+		write_permission_denied(path);
+		return (0);
+	}
+	return (1);
+}
+
+void			free_used_values(char *args1, char *args2, char **av)
+{
+	free_tab(av);
+	ft_free(&args1);
+	ft_free(&args2);
+}
+
 void			ms_execute(char *path, char *args, char **env)
 {
 	pid_t	pid;
@@ -38,19 +57,15 @@ void			ms_execute(char *path, char *args, char **env)
 	char	**av;
 	char	*args1;
 	char	*args2;
-	struct	stat fileStat;
 
 	errno = 0;
+	if (check_file_permission(path) == 0)
+		return ;
 	args2 = ft_strjoin(path, " ");
 	args1 = ft_strjoin(args2, args);
 	av = ft_split(args1, ' ');
 	x = 1;
-	if (stat(path , &fileStat) >=0 && ((fileStat.st_mode & S_IXUSR) == 0))
-	{
-		write_permission_denied(path);
-		return ;
-	}
-	if ((pid = fork()) == 0)
+	if (((pid = fork()) == 0))
 		x = execve(path, av, env);
 	if (x == -1)
 	{
@@ -60,7 +75,5 @@ void			ms_execute(char *path, char *args, char **env)
 	}
 	wait(&errno);
 	errno = errno / 255;
-	free_tab(av);
-	ft_free(&args1);
-	ft_free(&args2);
+	free_used_values(args1, args2, av);
 }
